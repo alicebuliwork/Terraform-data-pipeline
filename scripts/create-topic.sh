35fbe235ab8abbe450be2352fc3f992ed67d6f25
alice <alice@gmail.com>
2026-06-09 11:36:47 +0300

    create topic and connectors in the docker compose

--- scripts/create-topic.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker exec -it broker kafka-topics --create \
-  --topic cdc.public.orders \
-  --bootstrap-server broker:29092 \
-  --partitions 3 \
-  --replication-factor 1 || true
-
