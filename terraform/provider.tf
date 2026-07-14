de9479506cd86553c7bea4ce40e157b3a042c3e8
alicebuliwork <alicebuliwork@gmail.com>
2026-07-14 16:48:10 +0300

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0" # Downloads the latest 5.x version of the AWS plugin
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
