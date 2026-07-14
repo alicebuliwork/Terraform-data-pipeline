90ff35df7d9cb639110204af6d1469232442de48
alicebuliwork <alicebuliwork@gmail.com>
2026-07-14 16:55:23 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,13 @@
+module "iam"{
+  source = "./modules/iam"
+  user_name = "alice-user"
+}
+
 
 module "s3tables"{
   source = "./modules/s3tables"
   bucket_name = "alice-orders-table"
   namespace   = "orders"
-}
-module "iam"{
-  source = "./modules/iam"
-  user_name = "alice-user"
+
+  depends_on = [module.iam]
 }
