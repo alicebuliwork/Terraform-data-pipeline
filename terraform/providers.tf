511b771b407ee32d237f2fc951f599abc8990fe1
alicebuliwork <alicebuliwork@gmail.com>
2026-07-15 13:18:07 +0300

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0" # Downloads the latest 5.x version of the AWS plugin
+      version = "~> 6.0" # Downloads the latest 5.x version of the AWS plugin
     }
   }
 }
