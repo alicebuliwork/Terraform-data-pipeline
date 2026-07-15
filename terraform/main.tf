ea5db02dd11e3d11790c306de083a116a75ef18e
Alice Buli <327560611@gov.il>
2026-07-15 13:34:36 +0300

    add module

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,8 @@
-module "s3tables"{
-  source = "./modules/s3tables"
-  bucket_name = "alice-orders-table"
-  namespace   = "orders"
 
-  depends_on = [module.iam]
+
+module "ec2"{
+  source = "./modules/ec2"
+  ami_id = "ami-068b5bc67e48209c1"
+  tag = "try"
+  instance_type = "t2.micro"
 }
---
+++ terraform/modules/ec2/variables.tf
@@ -0,0 +1,11 @@
+variable "ami_id" {
+  type = string
+}
+
+variable "tag" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
