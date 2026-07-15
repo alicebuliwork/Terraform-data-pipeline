7ed5cf23e2f35c1c93bb5dbfe95d38d8fdcee1aa
Alice Buli <327560611@gov.il>
2026-07-15 12:15:44 +0300

    add pipeline

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,13 +5,6 @@ terraform {
       version = "~> 5.0" # Downloads the latest 5.x version of the AWS plugin
     }
   }
-  backend "s3" {
-    bucket         = "aws-alice-terraform-bucket"
-    key            = "terraform-data-pipeline/terraform.tfstate"
-    region         = "eu-north-1"
-    use_lockfile   = true
-    encrypt        = true
-  }
 }
 
 provider "aws" {
