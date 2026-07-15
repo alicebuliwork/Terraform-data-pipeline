737b50f5352415168cb22c86fec66b4b1dfb0652
Alice Buli <327560611@gov.il>
2026-07-15 11:06:29 +0300

    remove iam module

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "aws-alice-terraform-bucket"
-    key            = "terraform-data-pipeline/terraform.tfstate"
-    region         = "eu-north-1"
-    use_lockfile   = true
-    encrypt        = true
-  }
-}
