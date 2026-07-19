903d59c7fc4139f0896d347ce24fd0126a8006f5
Alice Buli <327560611@gov.il>
2026-07-19 12:16:15 +0300

    fix typo

--- terraform/modules/postgres_ec2/main.tf
+++ terraform/modules/postgres_ec2/main.tf
@@ -2,11 +2,11 @@ resource "aws_instance" "postgres_server" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
-  vpc_security_group_ids = var.sg_id
+  vpc_security_group_ids = [var.sg_id]
 
   # Startup Script to install and configure PostgreSQL
 
-  user_data = templatefile("${path.module}/user_data.sh", {
+  user_data = templatefile("./${path.module}/user_data.sh", {
     db_password = var.db_password
   })
 
